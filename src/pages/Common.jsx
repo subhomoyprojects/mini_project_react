5d0be6d1de2ef4cfc69075795a466124e913f510
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-06-14 00:10:23 +0530

    add design in contact

--- src/pages/Common.jsx
+++ src/pages/Common.jsx
@@ -2,17 +2,71 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import { useLocation } from "react-router";
 import TextField from "@mui/material/TextField";
+import { outlinedInputClasses } from "@mui/material/OutlinedInput";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+
+const customTheme = (outerTheme) =>
+  createTheme({
+    palette: {
+      mode: outerTheme.palette.mode,
+    },
+    components: {
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "--TextField-brandBorderColor": "#E0E3E7",
+            "--TextField-brandBorderHoverColor": "#B2BAC2",
+            "--TextField-brandBorderFocusedColor": "#6F7E8C",
+            "& label.Mui-focused": {
+              color: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
+          notchedOutline: {
+            borderColor: "var(--TextField-brandBorderColor)",
+          },
+          root: {
+            [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderHoverColor)",
+            },
+            [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+              borderColor: "var(--TextField-brandBorderFocusedColor)",
+            },
+          },
+        },
+      },
+    },
+  });
 
 function Common(props) {
   let checkLocation = useLocation();
-  console.log(checkLocation);
+  const outerTheme = useTheme();
   return (
     <>
       {checkLocation.pathname === "/contacts" ? (
         <form className="form-control">
-          <TextField id="outlined-basic" label="Name" variant="outlined" />
-          <TextField id="outlined-basic" label="Email" variant="outlined" />
-          <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: { sm: "1fr 1fr 1fr" },
+              gap: 2,
+            }}
+          >
+            <ThemeProvider theme={customTheme(outerTheme)}>
+              <TextField color="secondary" label="Name" variant="outlined" />
+              <TextField label="Email" variant="outlined" />
+              <TextField label="Phone Number" variant="outlined" />
+              <TextField label="Multiline" multiline rows={6} variant="outlined" className="fullwidth" />
+            </ThemeProvider>
+          </Box>
+          <Button className="btn" variant="contained" size="large">
+            Submit
+          </Button>
         </form>
       ) : (
         <Grid container spacing={8} className="design-controller">
