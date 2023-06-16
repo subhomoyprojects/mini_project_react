dd4f838597f88b972be3d737122cd5ac28909598
Subhomoy Pal <subhomoypal@Subhomoys-MacBook-Pro.local>
2023-06-16 19:44:24 +0530

    add form with condition

--- src/pages/Common.jsx
+++ src/pages/Common.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import { useLocation } from "react-router";
 import TextField from "@mui/material/TextField";
@@ -44,30 +44,66 @@ const customTheme = (outerTheme) =>
   });
 
 function Common(props) {
+  const [flag, setFlag] = useState(false);
+  const [inputEvents, setInputEvents] = useState({
+    fName: "",
+    email: "",
+    phoneNumber: "",
+    message: "",
+  });
   let checkLocation = useLocation();
   const outerTheme = useTheme();
+  // event handle onchange
+  const eventHandler = (event) => {
+    const { name, value } = event.target;
+    setInputEvents((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  };
+  // event handel submit
+  const submitValue = (e) => {
+    e.preventDefault();
+    setFlag((prevState) => {
+      if (inputEvents.fName !== "" && inputEvents.email !== "" && inputEvents.phoneNumber !== "") {
+        return (prevState = true);
+      }
+    });
+  };
   return (
     <>
       {checkLocation.pathname === "/contacts" ? (
-        <form className="form-control">
-          <Box
-            sx={{
-              display: "grid",
-              gridTemplateColumns: { sm: "1fr 1fr 1fr" },
-              gap: 2,
-            }}
-          >
-            <ThemeProvider theme={customTheme(outerTheme)}>
-              <TextField color="secondary" label="Name" variant="outlined" />
-              <TextField label="Email" variant="outlined" />
-              <TextField label="Phone Number" variant="outlined" />
-              <TextField label="Multiline" multiline rows={6} variant="outlined" className="fullwidth" />
-            </ThemeProvider>
-          </Box>
-          <Button className="btn" variant="contained" size="large">
-            Submit
-          </Button>
-        </form>
+        <>
+          <form className="form-control" onSubmit={submitValue}>
+            <Box
+              sx={{
+                display: "grid",
+                gridTemplateColumns: { sm: "1fr 1fr 1fr" },
+                gap: 2,
+              }}
+            >
+              <ThemeProvider theme={customTheme(outerTheme)}>
+                <TextField color="secondary" label="Name" variant="outlined" value={inputEvents.fName} name="fName" onChange={eventHandler} />
+                <TextField label="Email" variant="outlined" name="email" value={inputEvents.email} onChange={eventHandler} />
+                <TextField label="Phone Number" variant="outlined" name="phoneNumber" value={inputEvents.phoneNumber} onChange={eventHandler} />
+                <TextField label="Message" multiline rows={6} variant="outlined" value={inputEvents.message} className="fullwidth" name="message" onChange={eventHandler} />
+              </ThemeProvider>
+            </Box>
+            <Button className="btn" variant="contained" size="large" type="submit">
+              Submit
+            </Button>
+          </form>
+          {flag ? (
+            <div className="show-area">
+              <h4>Name: {inputEvents.fName}</h4>
+              <h4>Email: {inputEvents.email}</h4>
+              <h4>Phone Number: {inputEvents.phoneNumber}</h4>
+              <h4>Message: {inputEvents.message}</h4>
+            </div>
+          ) : null}
+        </>
       ) : (
         <Grid container spacing={8} className="design-controller">
           <Grid item xs={6} className="left-panel">
